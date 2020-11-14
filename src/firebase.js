2353b5e22a5ae9d9fde7ebd3aa5862b7c2e408ce
pakerchang <pakerchang@gmail.com>
2020-11-15 02:10:07 +0800

    readme.md
    Signed-off-by: pakerchang <pakerchang@gmail.com>

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,11 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: "AIzaSyCRMDMcZ6Clm8_joOQLy_ysvf3zdDNIjQI",
-	authDomain: "covid-19-tracker-e8035.firebaseapp.com",
-	databaseURL: "https://covid-19-tracker-e8035.firebaseio.com",
-	projectId: "covid-19-tracker-e8035",
-	storageBucket: "covid-19-tracker-e8035.appspot.com",
-	messagingSenderId: "949836568952",
-	appId: "1:949836568952:web:388a4decc83a96fc40908d",
-	measurementId: "G-XL1VTH6Y6N",
+	apiKey: "AIzaSyDjXe5KiTssnoPER1npIGSvDnx0kX3Dq4w",
+	authDomain: "covid-19-5e6c1.firebaseapp.com",
+	databaseURL: "https://covid-19-5e6c1.firebaseio.com",
+	projectId: "covid-19-5e6c1",
+	storageBucket: "covid-19-5e6c1.appspot.com",
+	messagingSenderId: "486708729952",
+	appId: "1:486708729952:web:2b87b94b159f0f6103cb05",
+	measurementId: "G-Z1R33BV2PT",
 };
--- src/util.js
+++ src/util.js
@@ -1,6 +1,8 @@
 import React from "react";
 import numeral from "numeral";
-import { Circle, Popup } from "react-leaflet";
+import { Circle, Popup, Marker } from "react-leaflet";
+import { AwesomeMarkers, icon } from "leaflet";
+
 
 const casesTypeColors = {
 	cases: {
@@ -40,14 +42,7 @@ export const prettyPrintStat = (stat) =>
 
 export const showDataOnMap = (data, casesType = "cases") =>
 	data.map((country) => (
-		<Circle
-			center={[country.countryInfo.lat, country.countryInfo.long]}
-			color={casesTypeColors[casesType].hex}
-			fillColor={casesTypeColors[casesType].hex}
-			fillOpacity={0.4}
-			radius={
-				Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
-			}>
+		<Marker position={[country.countryInfo.lat, country.countryInfo.long]} >
 			<Popup>
 				<div className="info__container">
 					<div
@@ -68,5 +63,35 @@ export const showDataOnMap = (data, casesType = "cases") =>
 					</div>
 				</div>
 			</Popup>
-		</Circle>
+		</Marker>
+
+		// <Circle
+		// 	center={[country.countryInfo.lat, country.countryInfo.long]}
+		// 	color={casesTypeColors[casesType].hex}
+		// 	fillColor={casesTypeColors[casesType].hex}
+		// 	fillOpacity={0.4}
+		// 	radius={
+		// 		Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+		// 	}>
+		// 	<Popup>
+		// 		<div className="info__container">
+		// 			<div
+		// 				className="info__flag"
+		// 				style={{
+		// 					backgroundImage: `url(${country.countryInfo.flag})`,
+		// 				}}
+		// 			/>
+		// 			<div className="info__countryName">{country.country}</div>
+		// 			<div className="info__confirmed">
+		// 				Case: {numeral(country.cases).format("0,0")}
+		// 			</div>
+		// 			<div className="info__recovered">
+		// 				Recovered: {numeral(country.recovered).format("0,0")}
+		// 			</div>
+		// 			<div className="info__deaths">
+		// 				Deaths: {numeral(country.deaths).format("0,0")}
+		// 			</div>
+		// 		</div>
+		// 	</Popup>
+		// </Circle>
 	));
