e34fd888d181e583c631882853e13c3ea910993b
pakerchang <pakerchang@gmail.com>
2020-11-15 03:47:40 +0800

    util.js casesTypeColors circle range
    Signed-off-by: pakerchang <pakerchang@gmail.com>

--- src/util.js
+++ src/util.js
@@ -1,19 +1,24 @@
 import React from "react";
 import numeral from "numeral";
+import L from "leaflet";
 import { Circle, Popup, Marker } from "react-leaflet";
-import { AwesomeMarkers, icon } from "leaflet";
 
+const myIcon = L.icon({
+	iconUrl: "my-icon.png",
+	iconSize: [38, 95],
+	iconAnchor: [22, 94],
+});
 
 const casesTypeColors = {
 	cases: {
 		hex: "#CC1034",
 		rgb: "rgb(204, 16, 52)",
-		multiplier: 800,
+		multiplier: 400,
 	},
 	recovered: {
 		hex: "#7dd71d",
 		rgb: "rgb(125, 215, 29)",
-		multiplier: 1200,
+		multiplier: 800,
 	},
 	deaths: {
 		hex: "#aaaaa",
@@ -42,37 +47,7 @@ export const prettyPrintStat = (stat) =>
 
 export const showDataOnMap = (data, casesType = "cases") =>
 	data.map((country) => (
-		<Marker position={[country.countryInfo.lat, country.countryInfo.long]} >
-			<Popup>
-				<div className="info__container">
-					<div
-						className="info__flag"
-						style={{
-							backgroundImage: `url(${country.countryInfo.flag})`,
-						}}
-					/>
-					<div className="info__countryName">{country.country}</div>
-					<div className="info__confirmed">
-						Case: {numeral(country.cases).format("0,0")}
-					</div>
-					<div className="info__recovered">
-						Recovered: {numeral(country.recovered).format("0,0")}
-					</div>
-					<div className="info__deaths">
-						Deaths: {numeral(country.deaths).format("0,0")}
-					</div>
-				</div>
-			</Popup>
-		</Marker>
-
-		// <Circle
-		// 	center={[country.countryInfo.lat, country.countryInfo.long]}
-		// 	color={casesTypeColors[casesType].hex}
-		// 	fillColor={casesTypeColors[casesType].hex}
-		// 	fillOpacity={0.4}
-		// 	radius={
-		// 		Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
-		// 	}>
+		// <Marker position={[34, -40]} icon={myIcon}>
 		// 	<Popup>
 		// 		<div className="info__container">
 		// 			<div
@@ -93,5 +68,35 @@ export const showDataOnMap = (data, casesType = "cases") =>
 		// 			</div>
 		// 		</div>
 		// 	</Popup>
-		// </Circle>
+		// </Marker>
+
+		<Circle
+			center={[country.countryInfo.lat, country.countryInfo.long]}
+			color={casesTypeColors[casesType].hex}
+			fillColor={casesTypeColors[casesType].hex}
+			fillOpacity={0.4}
+			radius={
+				Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
+			}>
+			<Popup>
+				<div className="info__container">
+					<div
+						className="info__flag"
+						style={{
+							backgroundImage: `url(${country.countryInfo.flag})`,
+						}}
+					/>
+					<div className="info__countryName">{country.country}</div>
+					<div className="info__confirmed">
+						Case: {numeral(country.cases).format("0,0")}
+					</div>
+					<div className="info__recovered">
+						Recovered: {numeral(country.recovered).format("0,0")}
+					</div>
+					<div className="info__deaths">
+						Deaths: {numeral(country.deaths).format("0,0")}
+					</div>
+				</div>
+			</Popup>
+		</Circle>
 	));
